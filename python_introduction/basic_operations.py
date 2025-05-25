f2bb65771a7199a32093bc02a95974dccf0392e4
Oyembo Peninah <aumaoyembo@gmail.com>
2025-05-25 16:01:09 +0300

    Add text file

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,10 @@
 number1 = 10
 number2 = 5
 print ("Addition of "number1" and "number2" is 15")
+print ("number1" + "number2")
 print ("Subtraction of "number1" and "number2" is 5")
+print ("number1 - "number2")
 print ("Multiplication of "number1" and "number2" is 50")
+print ("number1" * "number2")
+
 
