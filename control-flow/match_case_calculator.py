4f0a4f2c5de217e51a2396ea449aecc6b93c4fa3
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-01 19:28:30 +0300

    Add file edits

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+match operation:
+    case addition: 
+    addition = num1 + num2
+        print("addition") #Print The result is [addition]
+    case subtraction:
+    subtraction = num1 - num2
+        print("subtraction") #Print The result is [subtraction]
+    case multiplication:
+    multiplication = num1 * num2
+        print("multiplication") #Print The result is [multiplication]
+    case division:
+    division = num1 / num2
+        print("division") #Print The result is [division]
+    case _:
+        print("Cannot divide by zero")
+
+
+    
