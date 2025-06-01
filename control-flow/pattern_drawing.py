535f39a2d0f50ba4f74ce896953c1b52d63b69db
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-01 21:31:14 +0300

    Add new file edits

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,9 @@
+l = int(input("Enter the size of the pattern: ")) #Ensure value is positive integer
+size = l * *
+
+while l <=4:
+    print("size")
+        for l in range (1, 4)
+            print("*" , end="")
+            l = 4
+
