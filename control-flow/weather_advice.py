855517e27131a8ed346a9d44254f1c2350732867
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-01 18:02:26 +0300

    Add changes to weather

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+weather = input ("What's the weather like today? (sunny/rainy/cold): ")
+if weather is "sunny":
+    print("Wear a t-shirt and sunglasses.")
+if weather is "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+if weather is "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
