4729c534707811241f2da84df47659fba957fa7a
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-06 09:53:44 +0300

    Add recent edits to file

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,10 @@
 def perform_operation (operation):
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
+    add = num1 + num2
+    subtract = num1 - num2
+    multiply = num1 * num2
+    divide = num1 / num2
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     result = perform_operation(num1, num2, operation)
