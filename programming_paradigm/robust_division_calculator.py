54c68ccc95f6eb9146bb300538a1dced5353b856
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-15 20:03:11 +0300

    Add recent file edits

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,9 +1,11 @@
 def safe_divide(numerator, denominator):
-    if denominator == 0:
+    if denominator > 0:
+        return numerator / denominator
+    elif denominator == 0
         raise ZeroDivisionError ("Division by zero is not allowed")
     return safe_divide()
 
 try:
     if denominator == zero:
-        raise ValueError ("Input numeric values")
+        raise ValueError ("Please enter numeric values only")
     
