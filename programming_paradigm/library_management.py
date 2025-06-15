a907605a67e93a0f13c46be8e2adee579dab1678
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-15 23:09:08 +0300

    Add recent file edits

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,19 +2,22 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = Date 
+        self._is_checked_out = False
 class Library:
     def __init__(self):
         self._book = []
     def add_book(self, book):
         """Adds a book to library collection"""
+        self.add_book = Add Book
         library.add_book(Book("title", "author"))
     def check_out_book(self, title):
         """Check out book by title"""
+        self.check_out_book = Check out Book
         library.check_out_book("title")
         print("Available books after check out: ")
-    def return_book(self, title):
+    def return_book(self, "True"):
         """Return book to library by title"""
+        slef.return_book = Return Book
         print("Available book after return: ")
     def list_available_books(self):
         """Lists all book currently available in library"""
