62bd92f8c25907d6d37428bb22295cb4365e857b
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-15 19:08:30 +0300

    Add recent changes to file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,34 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        """This is the existing amount in bank account"""
+        if account_balance < 0:
+            print("Account balance cannot be negative. Initializing 0 balance.")
+            self.account_balance = 0
+            else:
+            print(f"Account opening balance: KES {self.account_balance:.2f}")
+ 
+
+    def display_balance (self, display_balance):
+        """Displays the existing account balance"""
+        print(f"Current balance: KES {self.account_balance:.2f}")
+
+    def deposit(self, amount):
+        """Deposits into the bank account"""
+        if amount > 0:
+            self.account_balance += amount
+            print(f"Amount Deposited: KES {self.amount:.2f}")
+            self.display_balance()
+
+    def withdraw(self, amount):
+        """Withdraws from bank account"""
+        if amount <= 0:
+            print("Negative withdrawal cannot is not allowed")
+        elif self.account_balance >= amount:
+            self.account_balance -= amount
+            print(f"Amount withdrawn: KES {self.amount:.2f}")
+        else:
+            print("Transcation Failed. Insufficient Funds!")
+            self.display_balance()
+
+
+        
