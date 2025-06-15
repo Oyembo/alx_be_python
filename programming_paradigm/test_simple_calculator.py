99c93ed8b2a3f8beec537ed0b09b478eb2226eea
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-15 21:59:39 +0300

    Add recent file edits

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,22 +2,24 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
-    def test_calculator(self, add, subtract, multiply, divide)
-    self.calculator = SimpleCalculator()
+    def test_calc(self, add, subtract, multiply, divide)
+    self.calc = SimpleCalculator()
     def test_addition(self):
         self.assertEqual(self.calculator.add(x, y))
         result = x + y
         return result
     def test_subtraction(self):
-        self.assertEqual(self.calculator.subtract(x, y))
+        self.assertEqual(self.calc.subtract(x, y))
         result = x - y
         return result
     def test_multiplication(self):
-        self.assertEqual(self.calculator.multiply(x, y))
+        self.assertEqual(self.calc.multiply(x, y))
         result = x * y
         return result
     def test_division(self):
-        self.assertEqual(self.calculator.divide(x, y))
+        self.assertEqual(self.calc.divide(x, y))
         result = x / y
         return result
+    except ZeroDivisionError ("Division by zero is not allowed")
+        return "Error: Cannot divide by zero"
 
