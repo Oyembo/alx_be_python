a543d0e9cc4b7d7a8f6ce984c6aa64dcc2607344
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-22 02:11:39 +0300

    Add recent edits

---
+++ oop/library_system.py
@@ -0,0 +1,25 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = str(title)
+        self.author = str(author)
+    
+class EBook:
+    def __init__(self, file_size):
+        self.file_size = int(file_size)
+
+class PrintBook:
+    def __init__(self, page_count):
+        self.page_count = int(page_count)
+
+books = Book("Book", "EBook", "PrintBook")
+    def add_book(self):
+        my_library = Book, EBook, PrintBook
+        return "Book, EBook, PrintBook"
+        print("my_library")
+    
+    def list_books(self):
+        print("my_library")
+    
+        
+
+
