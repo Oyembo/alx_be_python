301b29619da7d8d068491aace84b26ff215f3332
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-22 02:20:45 +0300

    Add recent file edits

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,22 +3,25 @@ class Book:
         self.title = str(title)
         self.author = str(author)
     
-class EBook:
+class EBook(Book): 
     def __init__(self, file_size):
+        super(). __init__(title, author)
         self.file_size = int(file_size)
 
-class PrintBook:
+class PrintBook(Book):
     def __init__(self, page_count):
+        super(). __init__(title, author)
         self.page_count = int(page_count)
 
-books = Book("Book", "EBook", "PrintBook")
-    def add_book(self):
-        my_library = Book, EBook, PrintBook
-        return "Book, EBook, PrintBook"
-        print("my_library")
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book: Book):
+        print(f"Added '{book.title}' to Library")
     
     def list_books(self):
-        print("my_library")
+        print(f"Books in {self.books}")
     
         
 
