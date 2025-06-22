fbef0b7be84d470c8b7cae8c1e3717a2c1220d15
Oyembo Peninah <aumaoyembo@gmail.com>
2025-06-22 03:19:56 +0300

    Add recent file edits

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,16 @@
+class Calculator:
+    def __init__(self, add):
+        self.add = add
+        
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    def __init__(self, multiply):
+        self.cls = multiply
+        return a * b
+    @classmethod
+    def display_count(cls):
+        print(f"{self.multiply}")
+
+
